2bf3f689f3b3072d548e40576f76946567eae018
ryanpolilen <45399061+ryanpolilen@users.noreply.github.com>
2018-11-27 12:37:56 -0500

    combine two arraylist
    combine two arraylist

---
+++ ArraylistGitHub.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class ArraylistGitHub{
+	public static void main(String[]args){
+
+	ArrayList<Integer> list1 = new ArrayList<>();
+	for (int i = 0; i <10;i++){
+		list1.add((int)(Math.random()*11)+10);
+	}
+		System.out.print(list1);
+		System.out.println();
+
+
+	ArrayList<Integer> list2 = new ArrayList<>();
+
+	for (int i = 0; i <10;i++){
+		list2.add((int)(Math.random()*11)+10);
+	}
+
+		System.out.print(list2);
+		System.out.println();
+		System.out.println(combineList(list1, list2));
+	}
+
+	public static ArrayList<Integer> combineList(ArrayList<Integer> list1, ArrayList<Integer> list2){
+
+
+			for (int i = 0; i < list2.size(); i++){
+				list1.add(list2.get(i));
+			}
+
+	return list1;
+	}
+}
